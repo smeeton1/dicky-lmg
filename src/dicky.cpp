fa7a0e657897380ee4133d184cfc385940924cce
smeeton1 <jeremy.p.rodriguez@mail.mcgill.ca>
2017-08-28 13:18:44 -0500

    H are the same

--- src/dicky.cpp
+++ src/dicky.cpp
@@ -17,7 +17,7 @@ main()
   unsigned int i,j,k,l;//counters
   
   // initializing variables 
-  Nmax=4; //qubit ensemble dimension must be even
+  Nmax=6; //qubit ensemble dimension must be even
   nmax=2*Nmax; //field dimension only even numbers
   Delta=1.0;eta=0.2;gamma=0.3;omega=1.0;omega0=1.0;
   tol=0.000001;
@@ -35,13 +35,12 @@ else{
 	f = - omega0*Nmax*(f + 1 /f)/4 + eta*Nmax/4;}
 	
   //here we are setting up the matrix for a^dagger a
-  for(i=0;i<nmax/2+1;i++){H(i,i)=omega*2*i;}
+  for(i=0;i<nmax/2+1;i++){H(i,i)=omega*2*i-f;}
   for(i=0;i<Nmax/2;i++){
    for(j=0;j<nmax+1;j++){
-     H(i*int(nmax+1)+j+int(nmax/2)+1,i*int(nmax+1)+j+int(nmax/2)+1)=omega*j-omega*alpha*alpha*(i+1)*(i+1);
+     H(i*int(nmax+1)+j+int(nmax/2)+1,i*int(nmax+1)+j+int(nmax/2)+1)=omega*j-omega*alpha*alpha*(i+1)*(i+1)-f;
    }
   }
-
   
   /*tm.set_size(int(Nmax/2)*(nmax+1),int(Nmax/2)*(nmax+1));//move this to the end only used for post processing
   for(i=0;i<Nmax/2-1;i++){
@@ -54,11 +53,11 @@ else{
   for(l=0;l<Nmax/2+1;l++){
     for(i=0;i<nmax+1;i++){
       for(j=0;j<nmax+1;j++){
-	 if((i+(l+1)*int(nmax+1)<size)&&(j+l*int(nmax+1)<size)){
+	 if((i+(l+1)*int(nmax+1)+(nmax/2)+1<size)&&(j+l*int(nmax+1)+(nmax/2)+1<size)){
 	  for(k=0;k<min(i,j)+1;k++){
-	    jp(i+(l+1)*int(nmax+1)-(nmax/2),j+l*int(nmax+1))+=exp(complex<double>((i+j-2*k)*log(alpha))+complex<double>(j-k)*log(complex<double>(-1))+complex<double>(0.5*(lgamma(i+1)+lgamma(j+1))-(lgamma(i-k+1)+lgamma(j-k+1)+lgamma(k+1)))); 
+	    jp(i+(l+1)*int(nmax+1)+(nmax/2)+1,j+l*int(nmax+1)+(nmax/2)+1)+=exp(complex<double>((i+j-2*k)*log(alpha))+complex<double>(j-k)*log(complex<double>(-1))+complex<double>(0.5*(lgamma(i+1)+lgamma(j+1))-(lgamma(i-k+1)+lgamma(j-k+1)+lgamma(k+1)))); 
 	  }
-	  jp(i+(l+1)*int(nmax+1)-(nmax/2),j+l*int(nmax+1))*=complex<double>(exp(-alpha*alpha/2))*sqrt(complex<double>(Nmax/2*(Nmax/2+1))-complex<double>((l+2)*(l+1)));
+	  jp(i+(l+1)*int(nmax+1)+(nmax/2)+1,j+l*int(nmax+1)+(nmax/2)+1)*=complex<double>(exp(-alpha*alpha/2))*sqrt(complex<double>(Nmax/2*(Nmax/2+1))-complex<double>((l+2)*(l+1)));
 	 }
   }}}
 
@@ -69,14 +68,15 @@ else{
 	}
 	jp0(i+int(nmax+1)-(nmax/2),j)*=complex<double>(exp(-alpha*alpha/2)*sqrt((Nmax/2*(Nmax/2+1))/ 2));
   }}
-  cout<<jp<<endl;
-  cout<<jp0<<endl;
+  cout<<"jp"<<jp<<endl;
+  cout<<"jp0"<<jp0<<endl;
   dJz=-(2*jp0.t()+2*jp0+jp+jp.t())/2;
-  H=H+Delta*dJz+eta/Nmax*dJz*dJz;
+  cout<<"jz*jz"<<(dJz*dJz)<<endl;
+  H=H+Delta*dJz+(eta/Nmax)*(dJz*dJz);
   cout<<H<<endl;
   cx_vec eigval;
   cx_mat eigvac;
-  eigs_gen(eigval, eigvac, H,int(10),"sm");
+  eigs_gen(eigval, eigvac, H,int(10),"lm");
   
   eigval=2*eigval/Nmax;
   ofstream fileeva("eigenval.dat");
@@ -86,7 +86,7 @@ else{
   ofstream fileeve("eigenvec.dat");
   for(i=0;i<eigvac.n_cols;i++){
     for(j=0;j<eigvac.n_rows;j++){
-      fileeve << conj(eigvac(j,i))*eigvac(j,i)<< " ";
+      fileeve << real(conj(eigvac(j,i))*eigvac(j,i))<< " ";
     }
     fileeve << "\n";
   }
