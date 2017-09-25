898cd32da5fe56b274d4b458d7eb8bf74facfddd
smeeton1 <jeremy.p.rodriguez@mail.mcgill.ca>
2017-09-25 16:59:46 -0500

    complied extra c++ code and add to ploting file

--- plot.py
+++ plot.py
@@ -8,6 +8,9 @@ import subprocess
 import matplotlib.pyplot as plt
 
 
+
+#################################################################################
+
 DoS = []
 f1 = open('results/DoS.dat', 'r')
 for line in f
@@ -15,3 +18,36 @@ for line in f
   DoS.append(double(data[1]),double(data[2]))
 
 f.close
+
+plt.figure(1)
+plt.plot(DoS,'.','b')
+plt.savefig('DoS.eps')
+
+del Dos
+
+#######################################################################################
+
+EiV = []
+f1 = open('results/eigenval.dat', 'r')
+for line in f
+  data=line.split()
+  EiV.append(double(data[1]))
+
+f.close
+
+mJz = []
+f1 = open('results/mjz.dat', 'r')
+for line in f
+  data=line.split()
+  mJz.append(double(data[1]))
+
+f.close
+
+plt.figure(1)
+plt.plot(EiV,mJz,'.','b')
+plt.savefig('PeresL.eps')
+
+del EiV
+del mJz
+
+#########################################################################################
