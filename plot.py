8f08332dccd82091e9eeff0870938bf4c36277fa
smeeton1 <jeremy.p.rodriguez@mail.mcgill.ca>
2017-10-24 13:48:08 -0500

    fixed a bug

--- plot.py
+++ plot.py
@@ -9,7 +9,10 @@ import matplotlib.pyplot as plt
 ##### Simple Numaricla intergration ################
 
 def IntPhi(b,a,omega,omega0,eta,gamma,i):
-  phi=(b-a)*(numpy.arccos(math.sqrt((omega*omega0)*((eta/(2*omega0))*pow(b,2)+b-i)/(2*pow(gamma,2)*(1-pow(b,2)))))+numpy.arccos(math.sqrt((omega*omega0)*((eta/(2*omega0))*pow(a,2)+a-i)/(2*pow(gamma,2)*(1-pow(a,2))))))/2
+  if b>a:
+    phi=(b-a)*(numpy.arccos(math.sqrt(abs((omega*omega0)*((eta/(2*omega0))*pow(b,2)+b-i)/(2*pow(gamma,2)*(1-pow(b,2))))))+numpy.arccos(math.sqrt(abs((omega*omega0)*((eta/(2*omega0))*pow(a,2)+a-i)/(2*pow(gamma,2)*(1-pow(a,2)))))))/2
+  else:
+    phi=(a-b)*(numpy.arccos(math.sqrt(abs((omega*omega0)*((eta/(2*omega0))*pow(b,2)+b-i)/(2*pow(gamma,2)*(1-pow(b,2))))))+numpy.arccos(math.sqrt(abs((omega*omega0)*((eta/(2*omega0))*pow(a,2)+a-i)/(2*pow(gamma,2)*(1-pow(a,2)))))))/2
   return phi
 
 
