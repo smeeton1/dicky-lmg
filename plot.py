db2f4a23c62778a78efa93f53ac5ee6fb6beccff
smeeton1 <jeremy.p.rodriguez@mail.mcgill.ca>
2017-10-25 12:52:16 -0500

    fixed a bug

--- plot.py
+++ plot.py
@@ -46,7 +46,7 @@ def DoSR1(xmin,xmax,e1,e2,omega,omega0,eta,gamma):
       h1=0
     zp=(math.sqrt(16*pow(gamma,4)+4*pow(gamma,2)*omega*(eta+2*i*omega0)+omega0*pow(omega,2)*(2*i*eta+omega0))+omega*omega0)/(4*pow(gamma,2)+eta*omega)
     z2=(math.sqrt(h1)-omega)/eta
-    phi=IntPhi(z2,z1,omega,omega0,eta,gamma,i)
+    phi=IntPhi(z2,zp,omega,omega0,eta,gamma,i)
     CDoS.append(0.5*((z2+1)/2+phi/math.pi))
     xdos.append(i)
     i+=0.001
