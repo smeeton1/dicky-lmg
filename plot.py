074f645ae10fa90dc162d1189589e0891dc2cce8
smeeton1 <jeremy.p.rodriguez@mail.mcgill.ca>
2018-08-27 13:40:26 -0500

    added command for remote use of matplotlib

--- plot.py
+++ plot.py
@@ -2,6 +2,8 @@ import sys
 import numpy 
 import cmath 
 import math 
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import scipy.integrate as integrate
